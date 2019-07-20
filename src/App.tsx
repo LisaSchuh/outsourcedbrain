43b4dac04e0eb5cca798a3f1bcf78a972c3f7906
Lisa Schuh <schuh.lisa@outlook.com>
2019-07-20 22:03:29 +0200

    added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 import { get, Note } from "./logic/noteService";
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  text-align: center;
+`;
+
+const AppLogo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.header`
+  color: #61dafb;
+`;
 
 const App: React.FC = () => {
   const notes = get();
@@ -9,12 +43,12 @@ const App: React.FC = () => {
     return notes.map(n => <li>{n.text}</li>);
   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <AppStyled>
+      <AppHeader>
+        <AppLogo src={logo} className="App-logo" alt="logo" />
+      </AppHeader>
       <ul>{renderNotes(notes)}</ul>
-    </div>
+    </AppStyled>
   );
 };
 
